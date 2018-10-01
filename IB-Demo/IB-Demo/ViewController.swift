93a7789a2355d2903ad98fe33c77303198c433b6
flaviolazzarini <flavio.lazzarini@stud.hslu.ch>
2018-10-01 15:52:18 +0200

    init

---
+++ IB-Demo/IB-Demo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  IB-Demo
+//
+//  Created by Flavio Lazzarini on 01.10.18.
+//  Copyright © 2018 Flavio Lazzarini. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
