249ebb1172e2dc15f8f3c837da0ee8c78836be97
Flavio Lazzarini <flavio@lazzarini.li>
2018-10-21 18:01:14 +0200

    Finished exc 05

--- AdaptiveApp/AdaptiveApp/ViewController.swift
+++ AdaptiveApp/AdaptiveApp/ViewController.swift
@@ -10,9 +10,11 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var lblProgrammer: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        self.lblProgrammer.text = NSLocalizedString("programer", comment: "Label: Title")
     }
 
 
