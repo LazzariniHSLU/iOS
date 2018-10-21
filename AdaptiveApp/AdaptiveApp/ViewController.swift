55f5af4972dd4286f6a0360d2fee90112ef065b8
Flavio Lazzarini <flavio@lazzarini.li>
2018-10-21 14:26:44 +0200

    Init exc 05

---
+++ AdaptiveApp/AdaptiveApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AdaptiveApp
+//
+//  Created by Flavio Lazzarini on 21.10.18.
+//  Copyright © 2018 Flavio Lazzarini. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
